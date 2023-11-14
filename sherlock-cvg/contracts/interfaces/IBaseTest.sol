129f4230f3793c1b41d63d151a8c30a6c78b8e20
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2023-11-14 14:29:21 +0200

    Delete scope directories

--- sherlock-cvg/contracts/interfaces/IBaseTest.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-import "./ICvgControlTower.sol";
-
-interface IBaseTest {
-    function initialize(ICvgControlTower _cvgControlTower) external;
-}
--- sherlock-cvg/contracts/interfaces/ICvgAssetStaking.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-interface ICvgAssetStaking {
-    // Deposit Principle token in Treasury through Bond contract
-    function processStakersRewards(uint256 amount) external;
-
-    function symbol() external view returns (string memory);
-}
