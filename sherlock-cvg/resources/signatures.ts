38ed5c915946c15876e20958c2a1c937f096ea3c
sherlock-admin <95431921+sherlock-admin@users.noreply.github.com>
2023-11-14 13:27:44 +0200

    Delete scope directories

--- sherlock-cvg/resources/signatures.ts
+++
@@ -1,4 +0,0 @@
-import {ethers} from "hardhat";
-
-export const WITHDRAW_SDT_BLACKHOLE = ethers.id("withdraw(address,uint256)").substring(0, 10);
-export const TRANSFER_ERC20 = ethers.id("transfer(address,uint256)").substring(0, 10);
